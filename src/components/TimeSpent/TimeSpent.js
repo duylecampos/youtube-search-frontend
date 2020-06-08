28cbc3fa36b1ada36f3f6965c36d1576ce1afb8d
Duyle Campos <duyle.campos@redevistorias.com.br>
2020-06-07 22:25:29 -0300

    feat(TimeSpent): improve component style

--- src/components/TimeSpent/TimeSpent.js
+++ src/components/TimeSpent/TimeSpent.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TimeSpent = ({ time }) => <div>{time}</div>;
+import styles from './TimeSpent.module.css';
+
+const TimeSpent = ({ time }) => (
+  <div className={styles.TimeSpent}>
+    <p>
+      Você levará <span>{time} dia(s)</span> para assistir todos os videos dessa
+      lista
+    </p>
+  </div>
+);
 
 TimeSpent.propTypes = {
   time: PropTypes.number.isRequired,
